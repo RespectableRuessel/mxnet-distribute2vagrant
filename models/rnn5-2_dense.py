63015b898bc8521843682ee780f3c323232f97a8
RespectableRuessel <fuckyougit@fuckyougit.com>
2019-07-16 20:47:31 +0200

    Fix sys args

--- models/rnn5-2_dense.py
+++ models/rnn5-2_dense.py
@@ -25,7 +25,7 @@ parser.add_argument('-t', '--test-split', required=True, type=float)
 
 args, unknown = parser.parse_known_args()
 
-name = sys.args[0].replace('.py', '')
+name = sys.argv[0].replace('.py', '')
 dir = os.path.dirname(args.dataset)
 
 print('%s %s' % (name, dir))
