3592b1f08f3c9fe4c1d5f8abd084390be154e18c
RespectableRuessel <fuckyougit@fuckyougit.com>
2019-07-16 20:50:38 +0200

    Change name to base file name

--- models/rnn5-2_dense.py
+++ models/rnn5-2_dense.py
@@ -25,7 +25,7 @@ parser.add_argument('-t', '--test-split', required=True, type=float)
 
 args, unknown = parser.parse_known_args()
 
-name = sys.argv[0].replace('.py', '')
+name = os.path.basename(sys.argv[0]).replace('.py', '')
 dir = os.path.dirname(args.dataset)
 
 print('%s %s' % (name, dir))
