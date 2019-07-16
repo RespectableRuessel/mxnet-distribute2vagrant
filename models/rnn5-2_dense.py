a7bde5c82b9925469c3943d56c0ae927cd7396ac
RespectableRuessel <fuckyougit@fuckyougit.com>
2019-07-16 20:58:54 +0200

    Use sparse label

--- models/rnn5-2_dense.py
+++ models/rnn5-2_dense.py
@@ -37,25 +37,26 @@ sequence_length = args.sequence_length
 test_size = args.test_split
 
 # kvstore
-kv = mx.kvstore.create('dist_sync')
+use_kv = False
+kv = None
+
+if use_kv:
+	kv = mx.kvstore.create('dist_sync')
 
 # load dataframe
 lines = sum(1 for line in open(args.dataset))
-df_len = lines // kv.num_workers
-df_skip = (df_len * kv.rank) - 1
+
+df_len = lines
+df_skip = 0
+
+if use_kv:
+	df_len = lines // kv.num_workers
+	df_skip = (df_len * kv.rank) - 1
+
 df = pd.read_csv(args.dataset, header = None, skiprows=df_skip, nrows = df_len)
 
 # extract targets
-y = []
-
-def to_vec(t):
-	if t > 0:
-		return np.array([0., 1.])
-	else:
-		return np.array([1., 0.])
-
-for t in df[df.columns[-1]]:
-	y.append(to_vec(t))
+y =  df[df.columns[-1]].to_numpy()
 
 # data preperation
 data_x = df.drop(df.columns[-1], axis=1).to_numpy()
@@ -98,8 +99,8 @@ train_metrics.add(train_metric_accuracy)
 train_metric_f1 = mx.metric.F1()
 train_metrics.add(train_metric_f1)
 
-#train_metric_perplexity = mx.metric.Perplexity(ignore_label=None)
-#train_metrics.add(train_metric_perplexity)
+train_metric_perplexity = mx.metric.Perplexity(ignore_label=None)
+train_metrics.add(train_metric_perplexity)
 
 test_metrics = mx.metric.CompositeEvalMetric()
 
@@ -109,14 +110,19 @@ test_metrics.add(test_metric_accuracy)
 test_metric_f1 = mx.metric.F1()
 test_metrics.add(test_metric_f1)
 
-#test_metricc_perplexity = mx.metric.Perplexity(ignore_label=None)
-#test_metrics.add(test_metricc_perplexity)
+test_metricc_perplexity = mx.metric.Perplexity(ignore_label=None)
+test_metrics.add(test_metricc_perplexity)
 
 # training
-l_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss(sparse_label=False)
-trainer = gluon.Trainer(net.collect_params(), 'adam', kvstore=kv, update_on_kvstore=True)
+l_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
+trainer = gluon.Trainer(net.collect_params(), 'adam')
+
+if use_kv:
+	trainer = gluon.Trainer(net.collect_params(), 'adam', kvstore=kv, update_on_kvstore=True)
 
 for epoch in range(epochs):
+
+	train_metrics.reset()
 	tic = time.time()
 
 	for i, (x, y) in enumerate(train_dataloader):
@@ -126,15 +132,21 @@ for epoch in range(epochs):
 
 			output = net(x)
 			loss = l_cross_entropy(output, y)
-
-		loss.backward()
-		train_metrics.update(output, y)
+			loss.backward()
 
 		trainer.step(x.shape[0])
 
+		train_metrics.update(y, output)
+
 	toc = time.time() - tic
 
-	print('[%s:%s] training completed in %ss with a accuracy of %s.' % (kv.rank, epoch, toc, train_metric.get()[1]))
+	if use_kv:
+		print('[%s:%s] training completed in %ss with a accuracy of %s.' % (kv.rank, epoch, toc, train_metric.get()[1]))
+	else:
+		print('[%s] training completed in %ss with a accuracy of %s.' % (epoch, toc, train_metric_accuracy.get()[1]))
+
+	test_metrics.reset()
+	tic = time.time()
 
 	for i, (x, y) in enumerate(test_dataloader):
 		# move to GPU if needed
@@ -142,13 +154,14 @@ for epoch in range(epochs):
 		y = y.as_in_context(ctx)
 
 		output = net(x)
-		test_metrics.update(output, y)
+		test_metrics.update(y, output)
 
 	toc = time.time() - tic
 
-	print('[%s:%s] testing completed in %ss with a accuracy of %s.' % (kv.rank, epoch, toc, test_metric.get()[1]))
-
-	tic = time.time()
+	if use_kv:
+		print('[%s:%s] testing completed in %ss with a accuracy of %s.' % (kv.rank, epoch, toc, test_metric.get()[1]))
+	else:
+		print('[%s] testing completed in %ss with a accuracy of %s.' % (epoch, toc, test_metric_accuracy.get()[1]))
 
 # checkpoint
 acc = test_metric_accuracy.get()[1]
